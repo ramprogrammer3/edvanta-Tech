07582f1614eece519e913763bc34204867d50c72
ram kumar <rammaniyari@gmail.com>
2024-02-22 08:56:35 +0530

    user login

--- insta-clone-backend/controllers/userController.js
+++ insta-clone-backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = async(req,res)=>{
     try {
@@ -55,6 +56,61 @@ exports.register = async(req,res)=>{
         return res.status(500).json({
             success : false,
             message : "Unable to register, please try again",
+        })
+    }
+}
+
+
+exports.login = async(req,res)=>{
+    try {
+        // fetch user input
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                success : false,
+                message : "All fields are required",
+            })
+        }
+
+        // check user is available or not
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({
+                success : false,
+                message : "User is not registerd, please register",
+            })
+        }
+
+        // compare password
+        const checkPassword = await bcrypt.compare(password,user.password);
+        if(!checkPassword){
+            return res.status(400).json({
+                success : false,
+                message : "Password is incorrect",
+            })
+        }
+
+        // create token
+        const token = jwt.sign({email : user.email,username : user.username,id : user._id},"sha256",{expiresIn : "30d"});
+        const options = {
+            httpOnly : true,
+        }
+
+        user.password = undefined;
+
+        // return response
+        return res.cookie("token",token,options).status(200).json({
+            success : true,
+            message : "Login successful",
+            token,
+            user
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            message : "Unable to login , please try again",
         })
     }
 }
--- insta-clone-backend/index.js
+++ insta-clone-backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const dbConnection = require("./config/database");
 const userRoutes = require("./routes/userRoute");
 
@@ -9,6 +10,7 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 const port = process.env.PORT || 4000;
 
